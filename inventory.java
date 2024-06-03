01c3115bb44df8a94e5ca4def7b58000b04c8354
Dylan L. <dylanl@hotmail.ca>
2024-06-03 13:54:31 -0400

    Started inv revamp

--- inventory.java
+++ inventory.java
@@ -9,8 +9,8 @@ public class inventory {
     public static String swrd = null;
     public static ArrayList<String> items = new ArrayList<String>();
     public static ArrayList<String> inv = new ArrayList<String>();
-    public static String s1 = null;
-    public static String s2 = null;
+    public static String rFalc = null;
+    public static String lArmour = null;
     public static String s3 = null;
     public static String s4 = null;
     public static String s5 = null;
@@ -23,14 +23,17 @@ public class inventory {
             return;
         }
         Object[] cont = { "continue", "Back to inventory" };
+        Object[] selections = items.toArray();
 
         // here is where our player choics are made.
-        swrd = JOptionPane.showInputDialog(
-                Arrays.toString(items.toArray()) + "\n write use 'ITEM NAME' in dialouge box to use item. no caps.");
+        String message = "Select the item you want to use";
+        int swrd = JOptionPane.showOptionDialog(null, message, "Rot and Deceit", JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.PLAIN_MESSAGE,
+                    null, selections, selections[0]);
         // because of time crunch, every item is hardcoded, and there's no checks to see
         // if you actually have said item. use that as you will.
-        if (swrd.equals("use rusty falcion")) {
-            if (items.contains(s1)){
+        if (swrd == 0) {
+            if (items.contains(rFalc)){
                 JOptionPane.showOptionDialog(null,
                     "You equip the falchion.\n An elegant blade, despite the specks of rust that\n coat it.",
                     "rot and deceit",
@@ -48,8 +51,8 @@ public class inventory {
 
         }
         // see? hardcoded. have fun cheesing items in.
-        else if (swrd.equals("use leather armour")) {
-            if (items.contains(s2)){
+        else if (swrd == 1) {
+            if (items.contains(lArmour)){
                 JOptionPane.showInputDialog(Arrays.toString(items.toArray())
                     + " You equip the Leather armour.\n it fits snugly over your body, \n feeling almost like a second skin.");
                 App.maxPHP = App.basePlayerHP + 10;
@@ -64,14 +67,14 @@ public class inventory {
                     JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, cont,
                     cont[0]);
             }
-        } else if (swrd.equals("use spear")) {
+        } else if (swrd == 2) {
             JOptionPane.showOptionDialog(null,
                     " You equip the castle gaurds' spear.\n it is mass produced trash, \n but it'll suffice.",
                     "rot and deceit",
                     JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, cont,
                     cont[0]);
             App.playerStr = App.Bstr + 5;
-        } else if (swrd.equals("use berries")) {
+        } else if (swrd == 3) {
             ;
             JOptionPane.showOptionDialog(null,
                     " You eat the berries, they taste delighfully...\n BY THE NINES! I SHOULD NOT HAVE EATEN THOSE \n I'M TWEAKING!!! I THINK I'M GOING TO DIE!",
