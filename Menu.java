e2acfa0e12f70bbbf463ed0c0feae11c839d9f37
Datt-boi <lukaszmm007@gmail.com>
2024-06-11 13:47:26 -0400

    Commit before merge
    please work...

--- Menu.java
+++ Menu.java
@@ -1,25 +1,36 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.DialogPane;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class Menu {
-
-    
-
-    @FXML
-    private Button but2;
+public class Menu extends Application {
 
     @FXML
-    private Button butext;
+    public static Button but1 = new Button("continue");
 
-    @FXML
-    private Button butinv;
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("fullscrn.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.UTILITY);
+        stage.setResizable(false);
+        stage.show();
+        stage.setScene(scene);
+        stage.show();
 
-    @FXML
-    private DialogPane dialouge;
+        }
 
-    @FXML
-    private AnchorPane plop;
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- SampleController.java
+++ SampleController.java
@@ -1,28 +1,46 @@
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
+import javafx.scene.layout.AnchorPane;
 
-    import javafx.fxml.FXML;
-    import javafx.scene.control.Button;
-    import javafx.scene.control.DialogPane;
-    import javafx.scene.layout.AnchorPane;
-    
-    public class SampleController {
-    
-        @FXML
-        public static Button but1 = new Button("cont");
-    
-        @FXML
-        private Button but2;
-    
-        @FXML
-        private Button butext;
-    
-        @FXML
-        private Button butinv;
-    
-        @FXML
-        private DialogPane dialouge;
-    
-        @FXML
-        private AnchorPane plop;
- }
+public class SampleController {
 
+    @FXML
+    public static Button but1;
+
+    @FXML
+    private void cont() {
+        System.out.println("penis");
+    }
+
+    @FXML
+    private Button but2;
+
+    @FXML
+    private void inv() {
+        System.out.println("balls");
+    }
+
+    @FXML
+    private Button butext;
+
+    @FXML
+    private void choice() {
+        System.out.println("Nutz");
+    }
+
+    @FXML
+    private Button butinv;
+
+    @FXML
+    private void plop() {
+        System.out.println("Shaft");
+    }
+
+    @FXML
+    private DialogPane dialouge;
+
+    @FXML
+    private AnchorPane plop;
+}
